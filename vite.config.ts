4b0c7ccaea671b7ffd3df4871724a3799f5a5edc
astik0398 <kumarastik0398@gmail.com>
2025-08-12 17:41:43 +0530

    fixing attempt for /blog route refresh

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-    base: './', // ✅ Added this line
+    base: '/', // ✅ Added this line
   server: {
     host: "::",
     port: 8080,
